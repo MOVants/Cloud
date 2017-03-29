19d527f8f9fe0b6eb9030cf02d84e4d3f293b724
MOVants <marvin.villaruel@safesat.com.ph>
2017-03-29 11:25:01 +0800

    1grf

---
+++ AndroidStudioProjects/CloudChat/app/src/main/java/qa/cloudchat/com/cloudchat/FireApp.java
@@ -0,0 +1,18 @@
+package qa.cloudchat.com.cloudchat;
+
+import android.app.Application;
+
+/**
+ * Created by marvin on 3/28/17.
+ */
+
+public class FireApp extends Application {
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+    }
+}
